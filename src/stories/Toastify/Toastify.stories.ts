ea407e23b66ce192b67ac10f91b09112cc832904
boakyezitek <kofi.boakye@eziteksolutions.com>
2023-08-15 21:40:28 +0000

    created toasify component and stories

---
+++ src/stories/Toastify/Toastify.stories.ts
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+
+import Toastify from '../../components/Toastify/Toastify.vue'
+
+const meta = {
+    title: "Zeelbee/Component/Toastify", 
+    component: Toastify,
+    tags: ['autodocs']
+} satisfies Meta<typeof Toastify>
+
+export default meta;
+
+type Story = StoryObj<typeof Toastify>
+
+export const ZeelbeeToastifyInfo:Story = {
+    args: {
+        type: 'info',
+        title:"Did you know?",
+        message: "Here’s something that you might like to know.",
+        showToast: true,
+    }
+}
+export const ZeelbeeToastifySuccess:Story = {
+    args: {
+        type: 'success',
+        title:"Yay! Everything worked!",
+        message: "Everything is up and runnig. You’re good to go.",
+        showToast: true,
+    }
+}
+export const ZeelbeeToastifyWarning:Story = {
+    args: {
+        type: 'warning',
+        title:"Uh oh, Something went wrong",
+        message: "Sorry! There was a problem with your request.",
+        showToast: true,
+    }
+}
+
+export const ZeelbeeToastifyDanger:Story = {
+    args: {
+        type: 'danger',
+        title:"Uh oh, Something went wrong",
+        message: "Sorry! There was a problem with your request.",
+        showToast: true,
+    }
+}
