bbcf7e613d980d71ab744a3d01866fc4dd21f2ad
Gyamfi John <115163376+gyamficodes@users.noreply.github.com>
2023-08-16 19:02:07 -0700

    update storybook version. Add clip card component and stories

---
+++ src/stories/ClipCard/ClipCard.stories.ts
@@ -0,0 +1,18 @@
+import { Meta , StoryObj } from '@storybook/vue3'
+
+import ClipCard from '../../components/ClipCard/ClipCard.vue' 
+
+const mata = {
+    title: 'Zeelbee/Component/ClipCard',
+    component: ClipCard,
+} satisfies Meta<typeof ClipCard>
+
+export default   mata
+type story = StoryObj<typeof ClipCard>
+
+export const  ZeelbeeClipCard: story = {
+    args: {
+       
+    }
+
+}
