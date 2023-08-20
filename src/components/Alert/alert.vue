99b1e1adc49d549dbb59d3630a88d993732929fd
Gyamfi John <115163376+gyamficodes@users.noreply.github.com>
2023-08-20 10:23:57 -0700

    redesign  alert.vue .

--- src/components/Alert/alert.vue
+++ src/components/Alert/alert.vue
@@ -1,6 +1,6 @@
 <template>
   
-  <div class=" h-[80px]  bg-zeel-primary  p-6 rounded-zeel-rounded-lg" :style="styles"><span class=" text-zeel-text-small  font-zeel-regular  text-white"  :style="styles">{{  title }}</span></div>
+  <div class=" h-[80px]  bg-zeel-primary  p-6 rounded-zeel-rounded-lg" :style="styles"><span class=" text-zeel-text-small  font-zeel-regular  text-white" :style="myColor" >{{  title }}</span></div>
     
 </template>
 
@@ -19,8 +19,15 @@ const props = withDefaults(
 
 
 const styles = computed(() => ({
-  background: props.type ===  "Primary" ? "#2D9CDB" : props.type === "Secondry" ? "#E1E4EA"  : props.type === "Warning" ? "#FFBE0A" : props.type === "Danger" ? "#FC5C65" : `${props.type}`,
-  color: props.type === "Secondry" ? '#000' : `${props.type}`
+  background: props.type ===  "Primary" ? "#f3f6ff" : props.type === "Secondry" ? "#E1E4EA"  : props.type === "Warning" ? "#Fdf4f4" : props.type === "Danger" ? "#fdf4f4" : `${props.type}`,
+  border: props.type === "Primary" ? " 1px solid" : props.type === "Secondry" ? " 1px solid" : props.type === "Warning" ? " 1px solid" : props.type === "Danger" ? " 1px solid" : `${props.type}`,
+  borderColor: props.type === "Primary" ? "#3366ff" :  props.type === "Warning"  ? "#ffb020"  :   props.type === "Danger" ? "red" :`${props.type}`,
+}))
+
+
+const myColor = computed(() => ({
+  color: props.type === "Primary" ? '#3366ff' :  props.type === "Warning" ? '#b99759' :   props.type === "Danger" ? "red" : `${props.type}`
+
 }))
 
 </script>
