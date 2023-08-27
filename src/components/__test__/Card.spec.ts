703df23cd4e4b51d655a10006d4b31bb4f549362
boakyezitek <kofi.boakye@eziteksolutions.com>
2023-08-27 05:18:16 +0000

    complete test case for card component

--- src/components/__test__/Card.spec.ts
+++ src/components/__test__/Card.spec.ts
@@ -4,9 +4,20 @@ import { mount } from '@vue/test-utils';
 import Card from '../Card/Card.vue';
 
 describe('Card.vue', () => {
-    const wrapper = mount(Card);
+    const wrapper = mount(Card, {
+        slots: { body: 'this is description'}
+    });
 
-    it('Expect card component to exit', () => {
+    it('component exit', () => {
         expect(wrapper.exists()).toBe(true);
     })
+
+    it('correct render slot', () => {
+        expect(wrapper.find('div').text()).toBe('this is description')
+    })
+
+    it('wrong slot render', () => {
+        expect(wrapper.find('div').text()).not.toBe('This is description')
+    })
+    
 })
